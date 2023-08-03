de83de280402f1fd85260e618d7a0e3d9d475b4b
Shubham Kshirsagar <103346490+shubham0454@users.noreply.github.com>
2023-08-03 23:00:02 +0530

    Delete javascript.js
    delete

--- javascript.js
+++
@@ -1,8 +0,0 @@
-
-var mainListDiv = document.getElementById("mainListDiv");
-mediaButton = document.getElementById('mediaButton');
-
-mediaButton.onclick = function () {
-  mainListDiv.classList.toggle('show_list');
-  mediaButton.classList.toggle('active');
-}
